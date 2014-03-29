4b0b77abf4dffa2b8bb9aed84dff4a02efe3bdf0
bouzuya <bouzuya@gmail.com>
2014-03-29 17:42:24 +0900

    support help & refactoring

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = require('./lib/');
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(robot) {
+  var dir = path.resolve(__dirname, 'lib/scripts');
+  fs.readdirSync(dir).forEach(function(file) {
+    robot.loadFile(dir, file);
+  });
+};
+
