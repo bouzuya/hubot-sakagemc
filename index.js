c3aaf67de448fdf29238b19350f88264cc85173c
bouzuya <m@bouzuya.net>
2014-08-09 18:24:06 +0900

    improve index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-var fs = require('fs');
 var path = require('path');
 
 module.exports = function(robot) {
-  var dir = path.resolve(__dirname, 'lib/scripts');
-  fs.readdirSync(dir).forEach(function(file) {
-    robot.loadFile(dir, file);
-  });
+  robot.load(path.join(__dirname, '/src/scripts/'));
 };
-
